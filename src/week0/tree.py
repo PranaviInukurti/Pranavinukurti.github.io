c71be8a2f375135beffd1e81ee6b4c7674bd3559
Rebecca-123 <rebecca.notw@gmail.com>
2022-03-29 15:32:11 -0700

    Add tree stump

--- src/week0/tree.py
+++ src/week0/tree.py
@@ -12,11 +12,22 @@ def tree():
         k = k - 1
         # inner loop to handle number of columns
         # values changing acc. to outer loop
-        for j in range(0, i+1):
+        for j in range(0, i + 1):
             # printing stars
             print("* ", end="")
         # ending line after each row
         print(" ")
+    # width of stump
+    trunk = n // 4
+    # rows of stump
+    trunk_height = n // 6
+    for i in range(trunk_height):
+        # trunk must be centered
+        for j in range(n - trunk):
+            print(" ", end="")
+        for k in range(trunk):
+            print("*", end="")
+        print("  ")
 
 
 if __name__ == "__main__":
