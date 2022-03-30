265dd9de69d944a0a604bef1070cf348b70a5013
Rebecca-123 <rebecca.notw@gmail.com>
2022-03-30 14:09:43 -0700

    GCD tester

--- src/week2/oop.py
+++ src/week2/oop.py
@@ -1,11 +1,37 @@
+import random
+
+
 class FindGCD:
     # All Python classes have a function called init(), which is always executed when the class is called and the
     # resulting object is constructed.
     def __init__(self):
-        self.num1 = num1
-        self.num2 = num2
+        pass
+
+    # Function to find the GCD of two Numbers
+    def __call__(self, num1, num2):
+        if num1 == 0:
+            return num1
+        while num2 != 0:
+            if num1 > num2:
+                num1 = num1 - num2
+            else:
+                num2 = num2 - num1
+        return num1
 
 
+# OOP Tester
+def gcdTester():
+    # run 20 tests
+    for i in range(20):
+        x = random.randrange(0, 200)
+        y = random.randrange(0, 500)
+        myInstance = FindGCD()
+        z = myInstance(x, y)
+        # print result of every test
+        print("The greatest common denominator of %d and %d is %d" % (x, y, z))
+
+
+# Imperative Form
 # Function to find the GCD of two Numbers
 def findgcd(num1, num2):
     if num1 == 0:
@@ -28,3 +54,4 @@ def gcd():
 
 if __name__ == "__main__":
     gcd()
+    gcdTester()
